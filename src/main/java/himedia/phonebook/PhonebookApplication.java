87dd2ed8c42df2c38cbb6f75491a73bde6a86452
kimyk0101 <kimyk7131@gmail.com>
2025-02-13 11:18:10 +0900

    Tomcat 배포를 위한 준비 작업

--- src/main/java/himedia/phonebook/PhonebookApplication.java
+++ src/main/java/himedia/phonebook/PhonebookApplication.java
@@ -3,6 +3,7 @@ package himedia.phonebook;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages="himedia.phonebook.mappers")
